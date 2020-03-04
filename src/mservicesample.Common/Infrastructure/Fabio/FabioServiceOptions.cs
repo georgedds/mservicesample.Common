224be936270a7040fda8d5b0b13c9e8815b66e2b
George Dedes <gdedes@vithoulkascompass.com>
2020-03-04 17:13:33 +0200

    Fabio extensions
    Fabio extensions

---
+++ src/mservicesample.Common/Infrastructure/Fabio/FabioServiceOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mservicesample.Common.Infrastructure.Fabio
+{
+    public class FabioServiceOptions
+    {
+        public bool Enabled { get; set; }
+        public Uri FabioAddress { get; set; }
+        public string ServiceName { get; set; }
+        public int RequestRetries { get; set; }
+    }
+}
